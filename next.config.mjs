d2289febceccaf3be61ca01fa3c341c69256bc54
Toan Thanh Nguyen (OkNguyen) <toan@oknguyen.com>
2026-06-14 02:32:45 +0700

    Thêm CI và Deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/crystal-blocks-3d",
+  assetPrefix: "/crystal-blocks-3d",
 };
 
 export default nextConfig;
